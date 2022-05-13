3ba9a97760351882556ff157d67db2fdb0fb0fee
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2022-05-13 01:06:14 -0300

    models update

--- cms/models.py
+++ cms/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    seo_title
-    seo_description
-    seo_keywords
     slug = models.CharField(max_length=120)
     title = models.CharField(max_length=120)
-    banner = models.CharField(max_length=120)
-    banner_alt = models.CharField(max_length=20)
-    banner_title = models.CharField(max_length=20)
-    updatedAt = models.CharField(max_length=20)
+    banner_src = models.CharField(max_length=120)
+    banner_alt = models.CharField(max_length=60)
+    banner_title = models.CharField(max_length=60)
+    updatedAt = models.CharField(max_length=30)
     categories = models.CharField(max_length=30)
     content = models.TextField()
+    seo_title  = models.CharField(max_length=120)
+    seo_description = models.TextField()
+    seo_keywords = models.TextField()
 
     def __str__(self):
         return self.title
--- cms/serializer.py
+++ cms/serializer.py
@@ -4,4 +4,19 @@ from cms.models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['id', 'slug', 'title', 'banner', 'categories', 'content']
+        fields = [
+            'id',
+            'slug',
+            'title',
+            'banner_src',
+            'banner_alt',
+            'banner_title',
+            'updatedAt',
+            'categories',
+            'content',
+            'seo_title',
+            'seo_description',
+            'seo_keywords',
+        ]
+
+        
